5915709eb931ac712e939d68b16244a906c24271
tasawwuramd <tasawwuramd@gmail.com>
2019-09-06 19:19:19 +0530

    Add files via upload

---
+++ requirejs-config.js
@@ -0,0 +1,19 @@
+var config = {
+    paths: {
+        'fixedNavbar': 'js/fixedNavbar',
+        'hoverProduct': 'js/hoverProduct',
+        'homePage': 'js/homePage'
+        
+    },
+    shim: {
+        'fixedNavbar': {
+            deps: ['jquery']
+        },
+        'hoverProduct':{
+            deps: ['jquery']
+        },
+        'homePage':{
+            deps: ['jquery']
+        }
+    }
+};
